6f2da08c462266fb92167221b7d02af5e241c3ea
Robert Gurtler <robert_gurtler@yahoo.com.au>
2017-05-27 23:35:57 +1000

    run puppet-lint over swarm.pp

--- modules/config/manifests/swarm.pp
+++ modules/config/manifests/swarm.pp
@@ -1,38 +1,36 @@
+# == Class: config:swarm
+
 class config::swarm {
 
-  class { 'docker_swarm': 
-  require => Class['config::consul_config']
+  class { 'docker_swarm':
+    require => Class['config::consul_config'],
   }
 
   docker_network { 'swarm-private':
-  ensure => present,
-  create => true, 
-  driver => 'overlay',
-  require => Class['config::consul_config']
+    ensure  => present,
+    driver  => 'overlay',
+    require => Class['config::consul_config'],
   }
 
-  if $hostname =~ /^swarm-master*/ {
-  
-  swarm_cluster {'cluster 1':
-    ensure       => present,
-    backend      => 'consul',
-    cluster_type => 'manage',
-    port         => '8500',
-    address      => '172.17.8.101',
-    advertise    => $::ipaddress_enp0s8,
-    path         => 'swarm',
-    }  
+  if $::hostname =~ /^swarm-master*/ {
+    swarm_cluster { 'cluster 1':
+      ensure       => present,
+      backend      => 'consul',
+      cluster_type => 'manage',
+      port         => '8500',
+      address      => '172.17.8.101',
+      advertise    => $::ipaddress_eth1,
+      path         => 'swarm',
+    }
   }
-
   else {
-
-  swarm_cluster {'cluster 1':
-   ensure       => present,
-   backend      => 'consul',
-   cluster_type => 'join',
-   port         => '8500',
-   address      => '172.17.8.101',
-   path         => 'swarm'
-   } 
+    swarm_cluster { 'cluster 1':
+      ensure       => present,
+      backend      => 'consul',
+      cluster_type => 'join',
+      port         => '8500',
+      address      => '172.17.8.101',
+      path         => 'swarm',
+    }
   }
-}  
+}
