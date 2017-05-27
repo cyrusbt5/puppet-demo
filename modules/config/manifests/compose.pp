6780adddeb0eb0084d4f4e19e4258fc25cbf72bc
Robert Gurtler <robert_gurtler@yahoo.com.au>
2017-05-27 23:08:33 +1000

    added sysctl to disable ipv6 testing puppet

--- modules/config/manifests/compose.pp
+++ modules/config/manifests/compose.pp
@@ -1,22 +1,56 @@
+# == Class: config::compose
+
 class config::compose {
-  
-if $hostname =~ /^swarm-master*/ {
- 
- notice ["This server is the Swarm Manager."]
 
-}
+  file_line { 'sysctl default disable ipv6':
+    ensure => present,
+    path   => '/etc/sysctl.conf',
+    line   => 'net.ipv6.conf.default.disable_ipv6 = 1',
+  }
 
-else {
+  file_line { 'sysctl all disable ipv6':
+    ensure => present,
+    path   => '/etc/sysctl.conf',
+    line   => 'net.ipv6.conf.all.disable_ipv6 = 1',
+  }
 
-file { '/root/docker-compose.yml':
-  ensure  => file,
-  content => template("config/registrator.yml.erb"), 
-  } ->
+  file_line { 'sysctl enable ipv4 forwarding':
+    ensure => present,
+    path   => '/etc/sysctl.conf',
+    line   => 'net.ipv4.conf.all.forwarding = 1',
+  }
 
-docker_compose {'swarm app':
-  ensure => present, 
-  source => '/root',  
-  scale => ['1']
+  exec { 'sysctl':
+    command     => '/sbin/sysctl -qp',
+    path        => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
+    subscribe   => [
+      File_line['sysctl default disable ipv6'],
+      File_line['sysctl all disable ipv6'],
+      File_line['sysctl enable ipv4 forwarding']
+    ],
+    refreshonly => true,
+  }
+
+  if $::hostname =~ /^swarm-master*/ {
+    notify { 'MASTER message':
+      message => 'This server is the Swarm Manager.'
+    }
+  }
+  else {
+
+    class {'docker::compose':
+      ensure  => present,
+      version => '1.8.1',
+    } ->
+
+    file { '/root/docker-compose.yml':
+      ensure  => file,
+      mode    => '0640',
+      content => template('config/registrator.yml.erb'),
+    } ->
+
+    docker_compose { '/root/docker-compose.yml':
+      ensure => present,
     }
   }
 }
