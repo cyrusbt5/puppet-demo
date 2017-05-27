fcfb7584f137ecfadbab72765969b9fd724b149d
Robert Gurtler <robert_gurtler@yahoo.com.au>
2017-05-27 23:13:43 +1000

    updated consul ip and testing contain class

--- modules/config/manifests/init.pp
+++ modules/config/manifests/init.pp
@@ -1,19 +1,28 @@
-# == Class: config
-#
-#
-class config(
-  
-  $consul_ip = "$::ipaddress_enp0s8", 
+# Class: config
+# ===========================
+
+class config (
+
+  $consul_ip = hiera('docker_swarm::backend_ip', '172.17.8.101')
 
 ){
-  
-  include config::consul_config 
-  contain config::swarm
-  contain config::compose
-  contain config::dns
-  contain config::run_containers
 
-  Class['config::swarm'] -> Class['config::compose'] -> Class['config::dns'] -> Class['config::run_containers']
+  class { 'config::dns': } ->
+  class { 'config::consul_config': } ->
+  class { 'config::swarm': } ->
+  class { 'config::compose': } ->
+  class { 'config::run_containers': }
+
+  contain 'config::dns'
+  contain 'config::consul_config'
+  contain 'config::swarm'
+  contain 'config::compose'
+  contain 'config::run_containers'
+
+  Class['config::dns'] ->
+  Class['config::consul_config'] ->
+  Class['config::swarm'] ->
+  Class['config::compose'] ->
+  Class['config::run_containers']
+
 }
-
-
